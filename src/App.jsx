8c7cee11cb772796858d88023c57259dee363e17
Shaiq Paracha <shaiqparacha123@gmail.com>
2024-01-18 11:46:41 +0000

    Updated Portfolio and github pages

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Muhammad Shaiq Paracha",
+  title: "Flutter Developer and Data Analyst",
+  email: "shaiqparacha123@gmail.com",
+  gitHub: "cyrus-programmer",
+  instagram: "shaiq_paracha",
+  linkedIn: "shaiq-paracha",
   medium: "",
-  twitter: "microsoft",
+  twitter: "shaiq_paracha",
   youTube: "Code",
 };
 
